b034a0ec887bc58dce528a88af1ceac6263cbf93
EppChops <chops@laptop.localdomain>
2021-12-07 14:59:07 +0100

    Finished day7

--- day7/day7.py
+++ day7/day7.py
@@ -1,7 +1,30 @@
 
-import heapq;
+import queue as Q
 
-h = []
-with open('init.txt', 'r') as f:
+pq = Q.PriorityQueue()
+
+def fuelcost(x, y):
+    steps = abs(x-y)
+    cost = (steps * (steps + 1))/2
+  
+    return cost
+
+
+with open('input.txt', 'r') as f:
     input = f.read().split(',')
     
+    inputArr = []
+    for i in input:
+        inputArr.append(int(i))
+    maxValue = max(inputArr)
+    for x in range(0,maxValue):
+        fuelCost = []
+        for y in inputArr:
+            #fuelCost.append(abs(x-y))
+            fuelCost.append(fuelcost(x,y))
+
+        pq.put(sum(fuelCost))
+
+
+
+    print(pq.get())
