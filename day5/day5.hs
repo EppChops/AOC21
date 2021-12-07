34eefcf326d81f6c888bab9463caecbcddd5f308
EppChops <chops@laptop.localdomain>
2021-12-07 10:12:35 +0100

    day5 finished

--- day5/day5.hs
+++ day5/day5.hs
@@ -1,20 +1,37 @@
 import Data.List.Split
+import qualified Data.HashMap as HM
 
 testInput = "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2"
 
 type Point = (Int, Int)
 type Line = (Point, Point)
 
-checkIsect :: [Line] -> [(Point, Int)]
-checkIsect (l:ls) = undefined
+ptsMap = HM.empty
 
-updatePoints :: [Point] -> [(Point, Int)]
-updatePoints (p:pts) = undefined2
+sol :: HM.Map Point Int -> Int
+sol m = length [ i |i <- HM.elems m, i > 1 ]
+
+readAndSolve fileP = do
+    input <- readFile fileP
+    print $ sol $ isect $ map readLine (lines input)
+
+isect :: [Line] -> HM.Map Point Int 
+isect ls = updatePoints $ concatMap points ls
+
+updatePoints :: [Point] -> HM.Map Point Int
+updatePoints []      = HM.empty
+updatePoints (p:pts) = HM.insertWith (\a b -> a + b) p 1 $ updatePoints pts
 
 points :: Line -> [Point]
-points ((x1, y1), (x2,y2)) 
+points ((x1, y1), (x2,y2))
+    | (x1,y1) == (x2,y2) = [(x2,y2)]
+    | otherwise = [(x1, y1)] ++ points ((x1 + signum (x2 - x1), y1 + signum (y2 - y1)),(x2, y2))
+
+points1 :: Line -> [Point]
+points1 ((x1, y1), (x2,y2)) 
     | (x1,y1) == (x2,y2) = [(x2,y2)]
     | y1 == y2 || x1 == x2 = [(x1, y1)] ++ points ((x1 + signum (x2 - x1), y1 + signum (y2 - y1)),(x2, y2))
+    | otherwise = []
                   
 readLine :: String -> Line
 readLine str = (p1, p2)
